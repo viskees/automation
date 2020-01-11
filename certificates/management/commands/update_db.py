4f109fbca30a701c25f99c00840feee6e4a6af91
viskees <viskees@gmail.com>
2020-01-11 19:32:37 +0100

    Update update_db.py

--- certificates/management/commands/update_db.py
+++ certificates/management/commands/update_db.py
@@ -1,81 +1,210 @@
 from django.core.management.base import BaseCommand
-from database.models import *
-from django.core.mail import send_mail
-import datetime
+from datetime import datetime
+import requests
+from database.models import BigIPNodes, Certificates, ProfileSSLClient, Database, VirtualServer
+from django.db.models import Q
 
 class Command(BaseCommand):
-    help = 'List certificates that are to be expired the upcoming month ' \
-           'List the top 10 to be expired certificates' \
-           'list the Luipaard clusters which are queried'
+    help = 'update the database of all cluster'
 
     def handle(self, *args, **kwargs):
+        # self.stdout.write(virtual_server.name)
 
-        #string waarin alle certificaten worden opgenomen die gaan verlopen
-        mail_body_certs_30dgn = ''
-        mail_body_certs_top10 = ''
-        mail_body_bigip_nodes = ''
-        counter = 0
+        # headers voor de API calls
+        headers = {'Authorization': 'Basic 123456', 'Content-Type': 'application/json'}
 
-        for virtual_server in VirtualServer.objects.select_related().order_by('profilesslclient__certificate__expiration').filter(profilesslclient_id__isnull=False):
-            #self.stdout.write(virtual_server.name)
+        for node in BigIPNodes.objects.all():
 
-            #test of het certificaat van de virtual server niet al verlopen is en of de verloopdatum niet meer dan een maand later is
-            if datetime.date.today() < virtual_server.profilesslclient.certificate.expiration.date() \
-                    and (virtual_server.profilesslclient.certificate.expiration.date() - datetime.date.today() < datetime.timedelta(days=30) ):
+            #print('updating: ' + node.bigip_ip)
 
-                mail_body_certs_30dgn += '\n' \
-                             + 'virtual server: ' + virtual_server.name \
-                             + ', op luipaardcluster: ' + virtual_server.bigip_name.bigip_name \
-                             + ', in partition: ' + virtual_server.partition \
-                             + ', met ssl client profile: ' +  virtual_server.profilesslclient.name \
-                             + ', en certificaat: ' + virtual_server.profilesslclient.certificate.name \
-                             + ', verloopt op: ' + str(virtual_server.profilesslclient.certificate.expiration.strftime("%d %b %Y %H:%M:%S")) \
-                             + '\n'
-            elif datetime.date.today() < virtual_server.profilesslclient.certificate.expiration.date() and counter < 10:
+            ########### Database eerst opschonen
 
-                mail_body_certs_top10 += '\n' \
-                             + 'virtual server: ' + virtual_server.name \
-                             + ', op luipaardcluster: ' + virtual_server.bigip_name.bigip_name \
-                             + ', in partition: ' + virtual_server.partition \
-                             + ', met ssl client profile: ' + virtual_server.profilesslclient.name \
-                             + ', en certificaat: ' + virtual_server.profilesslclient.certificate.name \
-                             + ', verloopt op: ' + str(virtual_server.profilesslclient.certificate.expiration.strftime("%d %b %Y %H:%M:%S")) \
-                             + '\n'
+            # omdat er geen timestamp beschikbaar is van de laatste configwijziging binnen het F5 cluster
+            # kan er niet worden bepaald of bepaalde tabellen moeten worden bijgewerkt.
+            # Daarom worden alle configuratietabellen die horen bij het betreffende F5 cluster opnieuw opgebouwd.
 
-                counter += 1
+            # verwijderen huidige tabel data voor de betreffende BigIP node
+            # queryset maken van alle data wat hoort bij de betreffende BigIP node
+            # achterhalen van het pkid van de BigIP node waarvoor een update wordt gevraagd
 
-            else:
-                continue
+            bigip_ip = node.bigip_ip
 
-        #overzicht van de clusters waarvoor het certificaatoverzicht is gemaakt.
+            bigip_node_id = BigIPNodes.objects.get(bigip_ip__exact=bigip_ip).id
 
-        bigip_node_list = BigIPNodes.objects.all()
-        for node in bigip_node_list:
-            mail_body_bigip_nodes += '\n' \
-                                     + node.bigip_name + ' (db update: ' + str(node.updated_on.strftime("%d %b %Y %H:%M:%S")) + ')'
-            #print(node.bigip_name)
+            # data verwijderen - certificates tabel
+            Certificates.objects.all().filter(bigip_name_id__exact=bigip_node_id).delete()
 
-        mail_body = "Hoi AS&D team," \
-                    + '\n \n' \
-                    + "Dit geautomatiseerde bericht geeft een overzicht van certificaten (op basis van client SSL profielen) welke de komende periode gaan verlopen. " \
-                    + "Het betreft certificaten op de clusters: "\
-                    + '\n' + mail_body_bigip_nodes \
-                    + '\n \n' \
-                    + "De volgende certificaten verlopen de komende 30 dagen: " \
-                    + '\n ' \
-                    + mail_body_certs_30dgn \
-                    + '\n \n' \
-                    + "Een top 10 overzicht van certificaten die de komende tijd gaan verlopen: " \
-                    + '\n ' \
-                    + mail_body_certs_top10 \
-                    + '\n \n' \
-                    + "Met vriendelijke groeten van de wi104366.ciman.nl" \
-                    + '\n \n' \
+            # data verwijderen - profileCSSLClient tabel
+            ProfileSSLClient.objects.all().filter(bigip_name_id__exact=bigip_node_id).delete()
 
-        mail_subject_date = datetime.date.today() + datetime.timedelta(days=7)
+            # data verwijderen - virtualserver tabel
+            VirtualServer.objects.all().filter(bigip_name_id__exact=bigip_node_id).delete()
 
-        mail_subject = 'AS&D certificaatverloopdata - volgende update volgt op: ' + str(mail_subject_date)
+            # data verwijderen - database tabel
+            Database.objects.all().filter(bigip_name_id__exact=bigip_node_id).delete()
 
-        #print(mail_body)
 
-        send_mail(mail_subject, mail_body, 'DictuCloudASD@dictu.nl', ['DictuCloudASD@dictu.nl', 'd.eshuis@dictu.nl', 'r.bakker@dictu.nl'])
+
+            ########### REST API calls
+
+            # certificaat details ophalen uit de opgegeven BigIP node
+            url = 'https://%s/mgmt/tm/sys/crypto/cert' % bigip_ip
+            certs = requests.get(url, headers=headers, verify=False)
+            certs_list_dict = certs.json()['items']
+
+            # client SSL profile details ophalen uit de opgegeven BigIP node
+            url = 'https://%s/mgmt/tm/ltm/profile/client-ssl' % bigip_ip
+            profile_cssl = requests.get(url, headers=headers, verify=False)
+            profile_cssl_list_dict = profile_cssl.json()['items']
+
+            # virtual server details ophalen uit de opgegeven BigIP node
+            # bij het wegschrijven naar de database worden ook nog API calls gedaan voor het achterhalen van de profielen
+            url = 'https://%s/mgmt/tm/ltm/virtual' % bigip_ip
+            virtual_servers = requests.get(url, headers=headers, verify=False)
+            virtual_servers_list_dict = virtual_servers.json()['items']
+
+
+            ########### Database bijwerken
+
+            #update op basis van deze queryset
+            BigIPNode = BigIPNodes.objects.get(bigip_ip=bigip_ip)
+
+            # certificaattabel
+
+            for cert_dict in certs_list_dict:
+
+                #print(cert_dict)
+
+                #bigip_name = BigIPNodes.objects.get(bigip_ip=bigip_ip)
+                #name = cert_name form cert_dict['name'].split("/")
+                #full_name = cert_dict['name']
+                #partition = cert_partition cert_dict['name'].split("/")
+                #expiration = cert_dict['apiRawValues']['expiration']
+                #commonName = cert_dict['commonName']
+                #certificateKeySize = cert_dict['apiRawValues']['certificateKeySize']
+                #publicKeyType = cert_dict['apiRawValues']['publicKeyType']
+                #organization = cert_dict['commonName']
+                #ou = cert_dict['ou']
+                #city = cert_dict['city']
+                #country = cert_dict['country']
+                #state = cert_dict['state']
+                #subjectAlternativeName = cert_dict['subjectAlternativeName']
+
+                slash, cert_partition, cert_name = cert_dict['name'].split("/")
+
+                BigIPNode.certificates_set.create(bigip_name = BigIPNodes.objects.get(bigip_ip=bigip_ip),
+                                                  name=cert_name,
+                                                  full_name=cert_dict['name'],
+                                                  partition=cert_partition,
+                                                  expiration=datetime.strptime(cert_dict['apiRawValues']['expiration'],
+                                                                               '%b %d %H:%M:%S %Y %Z'),
+                                                  commonName = cert_dict['commonName'] if 'commonName' in cert_dict.keys() else '',
+                                                  certificateKeySize = cert_dict['apiRawValues']['certificateKeySize'],
+                                                  publicKeyType = cert_dict['apiRawValues']['publicKeyType'],
+                                                  organization = cert_dict['organization'] if 'organization' in cert_dict.keys() else '',
+                                                  ou = cert_dict['ou'] if 'ou' in cert_dict.keys() else '',
+                                                  city = cert_dict['city'] if 'city' in cert_dict.keys() else '',
+                                                  country = cert_dict['country'] if 'country' in cert_dict.keys() else '',
+                                                  state = cert_dict['state'] if 'state' in cert_dict.keys() else '',
+                                                  subjectAlternativeName=cert_dict['subjectAlternativeName'] if 'subjectAlternativeName' in cert_dict.keys() else ''
+                                                      )
+
+            # client SSL tabel
+
+            for profile_cssl_dict in profile_cssl_list_dict:
+
+                # bigip_name = BigIPNodes.objects.get(bigip_ip=bigip_ip)
+                # certificate_id = Certificates.objects.get(full_name__exact=profile_cssl_dict['fullPath']).id
+                # full_name = profile_cssl_dict['fullPath']
+                # partition = profile_cssl_dict['partition']
+                # name = profile_cssl_dict['name']
+
+                certificate_id = Certificates.objects.get(full_name__exact=profile_cssl_dict['cert'],
+                                                          bigip_name_id__exact=bigip_node_id).id
+                BigIPNode.profilesslclient_set.create(certificate_id=certificate_id,
+                                                      full_name=profile_cssl_dict['fullPath'],
+                                                      partition=profile_cssl_dict['partition'],
+                                                      name=profile_cssl_dict['name'])
+
+
+            # virtual server tabel
+
+            for virtual_servers_dict in virtual_servers_list_dict:
+
+                # bigip_name = BigIPNodes.objects.get(bigip_ip=bigip_ip)
+                # profilesslclient_id = profilesslclient.objects.get(name__exact=profiles_dict['name']).id
+                # full_name = virtual_servers_dict['fullPath']
+                # partition = virtual_servers_dict['partition']
+                # name = virtual_servers_dict['name']
+                # destination = virtual_servers_dict['destination']
+                # profiles = profiles_dict['name']
+
+
+                # virtual server profielen toevoegen
+                # profile reference dictionary aanmaken
+                profile_reference = virtual_servers_dict.get('profilesReference')
+                profile_link_localhost = profile_reference.get('link')
+                profile_link_bigip_ip = profile_link_localhost.replace('localhost', bigip_ip)
+
+                # API call: create list of dictionaries of profiles which are assigned to virtual servers
+                profiles = requests.get(profile_link_bigip_ip, headers=headers, verify=False)
+                profiles_list_dict = profiles.json()['items']
+
+                profile_names_list = []
+
+                for profiles_dict in profiles_list_dict:
+                    profile_names_list.append(profiles_dict['name'])
+
+                #list to string
+                profile_names = ','.join(profile_names_list)
+
+                BigIPNode.virtualserver_set.create(full_name=virtual_servers_dict['fullPath'],
+                                                   name=virtual_servers_dict['name'],
+                                                   partition=virtual_servers_dict['partition'],
+                                                   destination=virtual_servers_dict['destination'],
+                                                   profiles=profile_names)
+
+
+            ########### Tabellen koppelen
+
+            # virtual server profilesslclient_id
+            # doorloop alle virtual servers
+            for virtualserver in VirtualServer.objects.filter(bigip_name_id__exact=bigip_node_id):
+
+                if virtualserver.profiles == '':
+
+                    # als er geen profielen zijn gekoppeld valt er niets te doen
+                    continue
+
+                else:
+
+                    # profilesslclient tabel query op basis van bigipname_id en partitie
+                    client_ssl_profiles = ProfileSSLClient.objects.filter(Q(partition__exact=virtualserver.partition)
+                                                                          | Q(partition__exact='Common'),
+                                                                          bigip_name_id__exact=virtualserver.bigip_name_id)
+
+                    # de gekoppelde profielenlijst doorlopen, opzoek naar een client ssl profiel
+                    for virtual_server_profile in virtualserver.profiles.split(','):
+
+                        #print("virtual server profile name: " + virtual_server_profile)
+
+                        for client_ssl_profile in client_ssl_profiles:
+
+                           #print("client ssl profile name: " + client_ssl_profile.name)
+
+                            if virtual_server_profile==client_ssl_profile.name:
+                                #match gevonden --> update virtual server tabel
+                                VirtualServer.objects.filter(name__exact=virtualserver.name,
+                                                                   bigip_name_id__exact=virtualserver.bigip_name_id,
+                                                                   partition__exact=virtualserver.partition).\
+                                    update(profilesslclient_id=client_ssl_profile.id)
+                                break
+                            else:
+                                continue
+
+
+
+
+            # database tabel bijwerken zodat het duidelijk is van welke datum de huidige configuratie is
+
+            BigIPNode.database_set.create()
