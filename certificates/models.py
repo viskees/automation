4e4b9783b51dbf96b72c24d04188b52329236f93
Kees Vis <viskees@gmail.com>
2020-02-10 17:05:31 +0100

    koppeling via irule en datagroup gereed voor testen

--- certificates/models.py
+++ certificates/models.py
@@ -35,3 +35,22 @@ class CertServerSSLVirtualServer(models.Model):
 
     def __str__(self):
         return self.cert_name
+
+class CertServerSSLVirtualServerViaIruleAndDatagroup(models.Model):
+    cert_name = models.CharField(max_length=200)
+    cert_partition = models.CharField(max_length=200)
+    cert_expiration = models.DateTimeField()
+    cert_cluster = models.CharField(max_length=200)
+    server_ssl_name = models.CharField(max_length=200)
+    server_ssl_partition = models.CharField(max_length=200)
+    irule_name = models.CharField(max_length=200)
+    datagroup_name = models.CharField(max_length=200)
+    vs_name = models.CharField(max_length=200)
+    vs_partition = models.CharField(max_length=200)
+    vs_ip = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['cert_expiration']
+
+    def __str__(self):
+        return self.cert_name
